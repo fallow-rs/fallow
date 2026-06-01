910471b122e267ac0cdc7859b7cefd4ab8f43bf6
Bart Waardenburg <bart@waardenburg.dev>
2026-06-01 14:08:44 +0200

    feat: resolve Nuxt script auto-imports

---
+++ tests/fixtures/nuxt-script-auto-imports/pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useExplicitThing } from '../composables/useExplicitThing';
+
+useCounter();
+formatPrice(10);
+fromIndex();
+sharedThing();
+useCollision();
+useExplicitThing();
+useRoute();
+
+const useLocalThing = () => null;
+useLocalThing();
+
+type Local = UseTypeOnly;
+</script>
+
+<template><div>index</div></template>
