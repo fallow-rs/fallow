1a2b01ebef9bf7c327615ce0b74cf2bbb4fd8184
Bart Waardenburg <bart@waardenburg.dev>
2026-06-25 22:32:46 +0200

    refactor: isolate napi health cli bridge

--- crates/napi/src/lib.rs
+++ crates/napi/src/lib.rs
@@ -8,7 +8,7 @@
 )]
 
 use fallow_api as api;
-use fallow_cli::programmatic::CliProgrammaticHealthRunner;
+use fallow_programmatic_cli::CliHealthRunner;
 use napi::bindgen_prelude::{AsyncTask, JsObjectValue, ToNapiValue, Unknown};
 use napi::{Env, ScopedTask, Status};
 use napi_derive::napi;
@@ -543,7 +543,7 @@ pub fn compute_complexity(
 ) -> napi::Result<AsyncTask<ProgrammaticTask>> {
     let options = api::ComplexityOptions::try_from(options.unwrap_or_default())?;
     Ok(AsyncTask::new(ProgrammaticTask::new(move || {
-        api::compute_complexity_with_runner(&options, &CliProgrammaticHealthRunner)
+        api::compute_complexity_with_runner(&options, &CliHealthRunner)
     })))
 }
 
@@ -553,7 +553,7 @@ pub fn compute_health(
 ) -> napi::Result<AsyncTask<ProgrammaticTask>> {
     let options = api::ComplexityOptions::try_from(options.unwrap_or_default())?;
     Ok(AsyncTask::new(ProgrammaticTask::new(move || {
-        api::compute_health_with_runner(&options, &CliProgrammaticHealthRunner)
+        api::compute_health_with_runner(&options, &CliHealthRunner)
     })))
 }
 
